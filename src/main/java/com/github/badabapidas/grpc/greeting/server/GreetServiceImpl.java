57bd928ce9f2c3dfe42d7693c68a6a3b2ceddc8c
Bapi Das <Bapi.Das@softwareag.com>
2019-08-26 21:33:46 +0530

    Added Unary Server impl

---
+++ src/main/java/com/github/badabapidas/grpc/greeting/server/GreetServiceImpl.java
@@ -0,0 +1,27 @@
+package com.github.badabapidas.grpc.greeting.server;
+
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
+import io.grpc.stub.StreamObserver;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+        // extract the fields from request
+        Greeting greeting = request.getGreeting();
+        String firstName = greeting.getFirstName();
+        String lastName = greeting.getLastName();
+
+        // create the response
+        String result ="Hello "+firstName+" "+lastName;
+        GreetResponse response = GreetResponse.newBuilder().setResult(result).build();
+
+        // send the response
+        responseObserver.onNext(response);
+
+        // complete the RPC call
+        responseObserver.onCompleted();
+    }
+}
